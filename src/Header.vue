b605a2863267535c0b9aad2d0f81d622f8e960e4
Pradip Khadka <fnfdrifter@gmail.com>
2018-03-22 17:19:20 +0545

    Header component

---
+++ src/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <header>
+      <h1>A Vue.js Markdown Previewer</h1>
+      <hr>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+
+</style>
