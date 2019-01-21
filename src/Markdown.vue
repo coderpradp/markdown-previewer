24097c99caf6bbeb887c2adb0b11acfc0f0c0b5b
Pradip Khadka <fnfdrifter@gmail.com>
2019-01-22 00:19:36 +0545

    File drop upload feature

--- src/Markdown.vue
+++ src/Markdown.vue
@@ -2,7 +2,11 @@
   <div id="content">
     <div class="editor">
       <h3 class="title">Markdown Editor</h3>
-      <textarea rows="20" v-model="markdownText"></textarea>
+      <textarea
+        rows="20"
+        v-model="markdownText"
+        id="dropzone"
+      ></textarea>
     </div>
     <div class="preview">
       <h3 class="title">Markdown Preview</h3>
@@ -12,7 +16,7 @@
 </template>
 
 <script>
-import marked from 'marked'
+import marked from "marked";
 
 export default {
   data() {
@@ -28,7 +32,7 @@ Itemized lists look like:
   * this one
   * that one
   * the other one`
-    }
+    };
   },
   computed: {
     previewText() {
@@ -41,14 +45,77 @@ Itemized lists look like:
         sanitize: true,
         smartLists: true,
         smartypants: false
-      })
-      return marked(this.markdownText)
+      });
+      return marked(this.markdownText);
+    }
+  },
+  methods: {
+    readFile(file) {
+      let reader = new FileReader();
+      reader.onload = e => {
+        let text = reader.result;
+        this.markdownText = text;
+      };
+      reader.readAsText(file);
+    }
+  },
+  mounted() {
+    let that = this;
+
+    let allowedExtensions = /(\.markdown|\.mdown|\.mkdn|\.mkd|\.md)$/i;
+    let dropArea = document.getElementById("dropzone");
+
+    ["dragenter", "dragover", "dragleave", "drop"].forEach(eventName => {
+      dropArea.addEventListener(eventName, preventDefaults, false);
+    });
+
+    function preventDefaults(e) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+
+    ["dragenter", "dragover"].forEach(eventName => {
+      dropArea.addEventListener(eventName, highlight, false);
+    });
+    ["dragleave", "drop"].forEach(eventName => {
+      dropArea.addEventListener(eventName, unhighlight, false);
+    });
+
+    function highlight(e) {
+      dropArea.classList.add("highlight");
+    }
+
+    function unhighlight(e) {
+      dropArea.classList.remove("highlight");
+    }
+
+    dropArea.addEventListener("drop", handleDrop, false);
+
+    function handleDrop(e) {
+      let dt = e.dataTransfer;
+      let file = dt.files[0];
+      if (!isValidFile(file)) {
+        return alert("Not a valid text or markdown file!");
+      }
+      that.readFile(file);
+    }
+
+    function isValidFile(file) {
+      if (file.type.indexOf("text") == 0) {
+        return true;
+      } else if (allowedExtensions.exec(file.name)) {
+        return true;
+      }
+      return false;
     }
   }
-}
+};
 </script>
 
 <style scoped>
+#dropzone.highlight {
+  background: lightblue;
+}
 #content {
   width: 90%;
   margin: 0 auto;
